f97223480914ef1257f9d767285fb63ab79009c2
Clément Férey <clement.ferey@gmail.com>
2015-01-21 16:03:40 +0100

    Initial commit

---
+++ yellow/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  yellow
+//
+//  Created by Clement on 21/01/2015.
+//  Copyright (c) 2015 isen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
