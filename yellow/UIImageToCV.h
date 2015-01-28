f5e2365494f1e2d07015419c2741cbfcb503965d
Clément Férey <clement.ferey@gmail.com>
2015-01-28 16:17:41 +0100

    Bridge between OpenCV and Swift added

---
+++ yellow/UIImageToCV.h
@@ -0,0 +1,23 @@
+//
+//  UIImageToCv.h
+//  yellow
+//
+//  Created by Clement on 28/01/2015.
+//  Copyright (c) 2015 isen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import <opencv2/opencv.hpp>
+
+@interface UIImage (OpenCV)
+
+//cv::Mat to UIImage
++ (UIImage *)imageWithCVMat:(const cv::Mat&)cvMat;
+- (id)initWithCVMat:(const cv::Mat&)cvMat;
+
+//UIImage to cv::Mat
+- (cv::Mat)CVMat;
+- (cv::Mat)CVMat3;  // no alpha channel
+- (cv::Mat)CVGrayscaleMat;
+
+@end
